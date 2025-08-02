a63c89d1210d5c9c46a9305bbe5661bb998d22a8
Ammar Asysyakur <asysyakurammar125@gmail.com>
2025-08-02 23:04:24 +0700

    Update variable.php

--- variable.php
+++ variable.php
@@ -12,21 +12,5 @@
  */
 
 // Praktik
-$nama = "John Doe"; // Variabel string
-echo $nama; // Output: John Doe
-
-/**
- * ========================
- * PENULISAN STRING: " vs '
- * ========================
- *
- * - Gunakan "double quotes" jika ingin menyisipkan variabel langsung di dalam string
- * - Gunakan 'single quotes' jika stringnya murni teks, tanpa perlu parsing variabel
- */
-
-// Praktik penggunaan double quotes (variabel akan dievaluasi)
-
-
-// Praktik penggunaan single quotes (variabel tidak akan dievaluasi)
 
 ?>
